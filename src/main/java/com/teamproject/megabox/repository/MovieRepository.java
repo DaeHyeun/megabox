dadc4617bc971eedbf8794611018a9f341d19c63
D Hyun <dh1236@naver.com>
2024-03-13 17:36:46 +0900

    패키징 완료

--- src/main/java/com/teamproject/megabox/repository/MovieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.teamproject.megabox.repository;
-
-import com.teamproject.megabox.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-
-}
